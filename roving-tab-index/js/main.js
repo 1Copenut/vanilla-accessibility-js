065a3f94115ddac9120edce3a78b3f78256c1931
1Copenut <trevor.pierce@elastic.co>
2022-02-28 17:16:00 -0600

    Renaming JS file in previous code directory.

--- roving-tab-index/js/main.js
+++
@@ -1,5 +0,0 @@
-import RovingTabindex from './modules/RovingTabindex.js';
-import RovingMultiselect from './modules/RovingMultiselect.js';
-
-const rovingTabindex = new RovingTabindex('#list-group-1', '.roving-list-item');
-const rovingMultiselect = new RovingMultiselect('#list-group-2', '.roving-multiselect-list-item');
