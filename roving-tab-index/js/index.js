6882e2a93ed80c381df65258cbd6dce2d6cd737c
1Copenut <trevor.pierce@elastic.co>
2022-02-28 17:16:30 -0600

    Adding index.js to roving-tab-index directory.

---
+++ roving-tab-index/js/index.js
@@ -0,0 +1,5 @@
+import RovingTabindex from './modules/RovingTabindex.js';
+import RovingMultiselect from './modules/RovingMultiselect.js';
+
+const rovingTabindex = new RovingTabindex('#list-group-1', '.roving-list-item');
+const rovingMultiselect = new RovingMultiselect('#list-group-2', '.roving-multiselect-list-item');
