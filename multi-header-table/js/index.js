e6638f7bcc988b894be3b8bce8a2e50ce4815de2
1Copenut <trevor.pierce@elastic.co>
2022-07-08 11:17:44 -0500

    Refactoring JS for personal preference.

--- multi-header-table/js/index.js
+++ multi-header-table/js/index.js
@@ -1,14 +1,16 @@
 // https://davidwalsh.name/javascript-debounce-function
-function _debounce(func, wait, immediate) {
+function _debounce(func, wait, immediate = false) {
   var timeout;
-  return function() {
-    var context = this,
-      args = arguments;
-    var later = function() {
+
+  return () => {
+    var context = this;
+    var args = arguments;
+    var later = () => {
       timeout = null;
       if (!immediate) func.apply(context, args);
     };
     var callNow = immediate && !timeout;
+
     clearTimeout(timeout);
     timeout = setTimeout(later, wait);
     if (callNow) func.apply(context, args);
@@ -16,30 +18,30 @@ function _debounce(func, wait, immediate) {
 }
 
 // https://inclusive-components.design/data-tables/
-function manageAriaScrollable(el) {
+function manageScrollableTable(el) {
+  console.log('Registered!');
+
   var el = document.getElementsByClassName(el)[0];
   var containerWidth = el.clientWidth;
   var scrollWidth = el.scrollWidth;
 
   if (scrollWidth > containerWidth) {
     el.setAttribute("tabindex", 0);
-    el.setAttribute("role", "group");
     el.classList.add("js-scroll");
   } else {
     el.removeAttribute("tabindex");
-    el.removeAttribute("role");
     el.classList.remove("js-scroll");
   }
 }
 
-var debouncedResize = _debounce(function() {
-  manageAriaScrollable("c-table__container");
+var debouncedResize = _debounce(() => {
+  manageScrollableTable("c-table__container");
 }, 500);
 
-window.addEventListener("DOMContentLoaded", function() {
-  manageAriaScrollable("c-table__container");
+window.addEventListener("DOMContentLoaded", () => {
+  manageScrollableTable("c-table__container");
 });
 
-window.addEventListener("resize", function() {
+window.addEventListener("resize", () => {
   debouncedResize();
 });
